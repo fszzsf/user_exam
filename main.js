b65a6db655ce79bfdddce9fb9f475094027a8aac
fayzulloh_devloper <pubgmobilehi0d@gmail.com>
2024-11-20 18:15:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,255 @@
+const movies = [
+  {
+    "Title": "in WWH",
+    "fulltitle": "in WWH (1993)",
+    "movie_year": 1993,
+    "Categories": "New",
+    "summary": "follow men with.",
+    "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+    "imdb_id": "t58427194",
+    "imdb_rating": 9.8,
+    "runtime": 91,
+    "language": "English",
+    "ytid": "Bt_YJdjbRM"
+  },
+  {
+    "Title": "fisher story",
+    "fulltitle": "fisher story (2024)",
+    "movie_year": 2024,
+    "Categories": "Drama",
+    "summary": "tough-love coach in hopes of winning the state championship.",
+    "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+    "imdb_id": "t59427194",
+    "imdb_rating": 7.8,
+    "runtime": 61,
+    "language": "English",
+    "ytid": "Bt_RYJdjbRM"
+  },
+  {
+    "Title": "artist",
+    "fulltitle": "artist (2010)",
+    "movie_year": 2010,
+    "Categories": "Sport",
+    "summary": "volleyball player Caroline \"Line\" Found, a team of dispirited high school girls must band together under the guidance of their tough-love coach in hopes of winning the state championship.",
+    "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+    "imdb_id": "t5427194",
+    "imdb_rating": 10,
+    "runtime": 161,
+    "language": "English",
+    "ytid": "Bt_RYJdjRM"
+  },
+    {
+      "Title": "The Miracle Season",
+      "fulltitle": "The Miracle Season (2018)",
+      "movie_year": 2018,
+      "Categories": "Drama|Sport",
+      "summary": "After the tragic death of star volleyball player Caroline \"Line\" Found, a team of dispirited high school girls must band together under the guidance of their tough-love coach in hopes of winning the state championship.",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt5427194",
+      "imdb_rating": 6.3,
+      "runtime": 101,
+      "language": "English",
+      "ytid": "Bt_RYJdjyRM"
+    },
+    {
+      "Title": "The Second",
+      "fulltitle": "The Second (2018)",
+      "movie_year": 2018,
+      "Categories": "Mystery|Thriller",
+      "summary": "The persona of a celebrated author is threatened when her best friend and muse reveals the dark secret behind her first novel's provenance, igniting an incendiary tale of sex, lies and betrayal.",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt5636922",
+      "imdb_rating": 5.1,
+      "runtime": 94,
+      "language": "English",
+      "ytid": "vjNVirPLT5I"
+    },
+    {
+      "Title": "The Princess Diaries",
+      "fulltitle": "The Princess Diaries (2001)",
+      "movie_year": 2001,
+      "Categories": "Comedy|Family|Romance",
+      "summary": "Mia Thermopolis is the average teenager - sweet, a little geeky and pretty much invisible to everyone with the exception of her mother, best friend Lilly and Lilly's older brother Michael. Making it through high school without throwing up is a challenge in itself for Mia, so it doesn't come as welcome news when her estranged grandmother shows up out of the blue and calmly informs her that she is in fact the heir to the throne of a European country called Genovia. Suddenly Mia's life is thrown into complete overload. She's being taught about scarves, waves and pears in order to become a perfect princess, she gets a makeover and a tough looking yet sweet bodyguard/limo driver called Joe. Things get out of hand when the media gets a hold of the story and suddenly Mia is thrust into the spotlight in both the newspapers and in school. On top of all that Mia has a choice to make. She must decide by Genovia's Independence Day Ball whether she longs to relinquish her claim on the throne or to...",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt0247638",
+      "imdb_rating": 6.3,
+      "runtime": 111,
+      "language": "English",
+      "ytid": "2CkcwPi20ms"
+    },
+    {
+      "Title": "March of the Penguins",
+      "fulltitle": "March of the Penguins (2005)",
+      "movie_year": 2005,
+      "Categories": "Documentary|Family",
+      "summary": "At the end of each Antarctic summer, the emperor penguins of the South Pole journey to their traditional breeding grounds in a fascinating mating ritual that is captured in this documentary by intrepid filmmaker Luc Jacquet. The journey across frozen tundra proves to be the simplest part of the ritual, as after the egg is hatched, the female must delicately transfer it to the male and make her way back to the distant sea to nourish herself and bring back food to her newborn chick.",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt0428803",
+      "imdb_rating": 7.6,
+      "runtime": 80,
+      "language": "English",
+      "ytid": "JZWt5YkhVp8"
+    },
+    {
+      "Title": "A Silent Voice",
+      "fulltitle": "A Silent Voice (2016)",
+      "movie_year": 2016,
+      "Categories": "Animation|Drama|Family|Romance",
+      "summary": "The story revolves around Nishimiya Shoko, a grade school student who has impaired hearing. She transfers into a new school, where she is bullied by her classmates, especially Ishida Shouya. It gets to the point where she transfers to another school and as a result, Shouya is ostracized and bullied himself, with no friends to speak to and no plans for the future. Years later, he sets himself on a path to redemption.",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt5323662",
+      "imdb_rating": 8.2,
+      "runtime": 130,
+      "language": "English",
+      "ytid": "twUSlecQpGQ"
+    },
+    {
+      "Title": "The Son of Bigfoot",
+      "fulltitle": "The Son of Bigfoot (2017)",
+      "movie_year": 2017,
+      "Categories": "Animation|Comedy|Family|Fantasy",
+      "summary": "Teenage outsider Adam sets out on an epic and daring quest to uncover the mystery behind his long-lost dad, only to find out that he is none other than the legendary Bigfoot! He has been hiding deep in the forest for years to protect himself and his family from HairCo., a giant corporation eager to run scientific experiments with his special DNA. As father and son start making up for lost time after the boy's initial disbelief, Adam soon discovers that he too is gifted with superpowers beyond his imagination. But little do they know, HairCo. is on their tail as Adam's traces have led them to Bigfoot!",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt5715410",
+      "imdb_rating": 6.1,
+      "runtime": 92,
+      "language": "English",
+      "ytid": "TKNYU_M6O5Y"
+    },
+    {
+      "Title": "The Princess Diaries 2: Royal Engagement",
+      "fulltitle": "The Princess Diaries 2: Royal Engagement (2004)",
+      "movie_year": 2004,
+      "Categories": "Comedy|Family|Romance",
+      "summary": "Princess Mia has just turned 21 and is supposed to succeed her grandmother as the Queen of Genovia. But Viscount Mabrey who wishes that his nephew who is also in line to the throne to be the new ruler, reminds everyone of a law that states that an unmarried woman can't be made queen, and with the backing of parliament, he opposes Mia's coronation. But Queen Clarice asks that Mia be allowed time to find a husband, and she is given 30 days. But Mabrey tries to do what he can to stop that. But his nephew, Nicholas has met Mia and they are both attracted to each other but Mia upon learning who he is, dislikes and doesn't trust him but Clarice has invited him to stay with them for the 30 day period to keep an eye on him.",
+      "ImageURL": " https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt0368933",
+      "imdb_rating": 5.8,
+      "runtime": 113,
+      "language": "English",
+      "ytid": "iemU7EUQgmo"
+    },
+    {
+      "Title": "Strange Magic",
+      "fulltitle": "Strange Magic (2015)",
+      "movie_year": 2015,
+      "Categories": "Adventure|Animation|Comedy|Family|Fantasy",
+      "summary": "Strange Magic,\" a new animated film, is a madcap fairy tale musical inspired by \"A Midsummer Night's Dream.\" Popular songs from the past six decades help tell the tale of a colorful cast of goblins, elves, fairies and imps, and their hilarious misadventures sparked by the battle over a powerful potion.",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt4191054",
+      "imdb_rating": 5.8,
+      "runtime": 99,
+      "language": "English",
+      "ytid": "Xr9iLfD6xus"
+    },
+    {
+      "Title": "Adrift",
+      "fulltitle": "Adrift (2018)",
+      "movie_year": 2018,
+      "Categories": "Adventure",
+      "summary": "Starring Shailene Woodley and Sam Claflin, Adrift is based on the inspiring true story of two free spirits whose chance encounter leads them first to love, and then to the adventure of a lifetime. As the two avid sailors set out on a journey across the ocean, Tami Oldham (Woodley) and Richard Sharp (Claflin) couldn't anticipate they would be sailing directly into one of the most catastrophic hurricanes in recorded history. In the aftermath of the storm, Tami awakens to find Richard badly injured and their boat in ruins. With no hope for rescue, Tami must find the strength and determination to save herself and the only man she has ever loved. Adrift is the unforgettable story about the resilience of the human spirit and the transcendent power of love.",
+      "ImageURL": "https://cdn.marvel.com/content/1x/thunderbolts_lob_crd_03.jpg",
+      "imdb_id": "tt6306064",
+      "imdb_rating": 6.7,
+      "runtime": 96,
+      "language": "English",
+      "ytid": "n9ukI7khQpE"
+    }
+  ];
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------------------
+
+
+
+
+
+// const continir = document.querySelector('#cardsCon')
+// continir.map(item => {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
+  
+//     card.innerHTML = `
+//       <img src="${item.ImageURL}" alt="">
+//       <h3>${item.Title}</h3>
+//       <p>${item.movie_year}</p>
+//       <p>Rating: ${item.Categories}</p>
+//       <p>Duration: ${item.imdb_rating}</p>
+//       <button>More info</button>
+//     `;
+  
+// });
+
+
+
+
+    const container = document.getElementById("movie-container");
+    const searchBox = document.getElementById("search-box");
+    const genreFilter = document.getElementById("genre-filter");
+    const ratingFilter = document.getElementById("rating-filter");
+  
+ 
+    const renderMovies = (moviesAS) => {
+    container.innerHTML = "";
+  
+      moviesAS.forEach(movie => {
+
+      const card = document.createElement("div");
+      card.className = "card";
+  
+
+      const img = document.createElement("img");
+      img.src = movie.ImageURL;
+      card.appendChild(img);
+  
+      const title = document.createElement("h3");
+      title.textContent = movie.Title;
+      card.appendChild(title);
+        
+      const hr = document.createElement("hr")
+      card.appendChild(hr);
+
+      const full = document.createElement("h3")
+      full.textContent = movie.fulltitle;
+      card.appendChild(full);
+
+
+      const details = document.createElement("p");
+      details.textContent = `Year: ${movie.movie_year} | Rating: ${movie.imdb_rating} | Run-Time: ${movie.runtime}min`;
+      card.appendChild(details);
+      const button = document.createElement("button");
+      button.textContent = "More info";
+      card.appendChild(button);
+  
+      container.appendChild(card);
+    });
+  };
+ 
+
+  renderMovies(movies);
+
+  searchBox.addEventListener("input", (e) => {
+    const filteredMovies = movies.filter(item =>
+      item.Title[0].toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    renderMovies(filteredMovies);
+    console.log(filteredMovies)
+
+  });
+  genreFilter.addEventListener('change', function (){
+    const filteredMovies = movies.filter(item => item.Categories.includes(this.value))
+    renderMovies(filteredMovies)
+  })
+  ratingFilter.addEventListener('change', function (){
+    const filteredMovies = movies.filter(item => item.imdb_rating >= this.value)
+    renderMovies(filteredMovies)
+  })
